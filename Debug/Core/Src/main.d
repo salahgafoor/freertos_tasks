c25b1e79a8c4c0252b9f5411343a5428869ffe1b
Salah Gafoor <sgafoor@sensata.com>
2024-02-04 03:58:24 +0530

    Pre-emptive scheduling prints uneven output from two tasks

--- Debug/Core/Src/main.d
+++ Debug/Core/Src/main.d
@@ -25,7 +25,16 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_pwr.h \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_pwr_ex.h \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim.h \
- ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim_ex.h
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim_ex.h \
+ C:/Private/stm32/STM_Workspace/RTOS_Workspace/freertos_tasks/ThirdParty/freertos/include/FreeRTOS.h \
+ C:/Private/stm32/STM_Workspace/RTOS_Workspace/freertos_tasks/ThirdParty/freertos/FreeRTOSConfig.h \
+ C:/Private/stm32/STM_Workspace/RTOS_Workspace/freertos_tasks/ThirdParty/freertos/include/projdefs.h \
+ C:/Private/stm32/STM_Workspace/RTOS_Workspace/freertos_tasks/ThirdParty/freertos/include/portable.h \
+ C:/Private/stm32/STM_Workspace/RTOS_Workspace/freertos_tasks/ThirdParty/freertos/include/deprecated_definitions.h \
+ C:/Private/stm32/STM_Workspace/RTOS_Workspace/freertos_tasks/ThirdParty/freertos/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Private/stm32/STM_Workspace/RTOS_Workspace/freertos_tasks/ThirdParty/freertos/include/mpu_wrappers.h \
+ C:/Private/stm32/STM_Workspace/RTOS_Workspace/freertos_tasks/ThirdParty/freertos/include/task.h \
+ C:/Private/stm32/STM_Workspace/RTOS_Workspace/freertos_tasks/ThirdParty/freertos/include/list.h
 ../Core/Inc/main.h:
 ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal.h:
 ../Core/Inc/stm32g4xx_hal_conf.h:
@@ -54,3 +63,12 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_pwr_ex.h:
 ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim.h:
 ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim_ex.h:
+C:/Private/stm32/STM_Workspace/RTOS_Workspace/freertos_tasks/ThirdParty/freertos/include/FreeRTOS.h:
+C:/Private/stm32/STM_Workspace/RTOS_Workspace/freertos_tasks/ThirdParty/freertos/FreeRTOSConfig.h:
+C:/Private/stm32/STM_Workspace/RTOS_Workspace/freertos_tasks/ThirdParty/freertos/include/projdefs.h:
+C:/Private/stm32/STM_Workspace/RTOS_Workspace/freertos_tasks/ThirdParty/freertos/include/portable.h:
+C:/Private/stm32/STM_Workspace/RTOS_Workspace/freertos_tasks/ThirdParty/freertos/include/deprecated_definitions.h:
+C:/Private/stm32/STM_Workspace/RTOS_Workspace/freertos_tasks/ThirdParty/freertos/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Private/stm32/STM_Workspace/RTOS_Workspace/freertos_tasks/ThirdParty/freertos/include/mpu_wrappers.h:
+C:/Private/stm32/STM_Workspace/RTOS_Workspace/freertos_tasks/ThirdParty/freertos/include/task.h:
+C:/Private/stm32/STM_Workspace/RTOS_Workspace/freertos_tasks/ThirdParty/freertos/include/list.h:
